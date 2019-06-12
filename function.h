d91d95aba31d7c73635734a0f5908ca5abcdb604
Victor <victor.huanqui@usach.cl>
2019-06-12 13:18:29 -0400

    B

--- function.h
+++ function.h
@@ -10,6 +10,7 @@ Structs
 
 
 
+
 float partialRealAverage(visibility_s *buffer, int lenBuffer, int *quantityPReal, double realAverage);
 
 float partialImaginaryAverage(visibility_s *buffer, int lenBuffer, int *quantityPImaginary, double imaginaryAverage);
